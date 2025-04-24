0071ec480fa0028c9cf91825b81b07e40f31c350
MikeTheMichaelS <yiyunsun@bu.edu>
2025-04-24 15:13:37 -0400

    add app structure

---
+++ app/printer/page.tsx
@@ -0,0 +1,9 @@
+import Printer from "@/components/Printer";
+
+export default function PrintCode() {
+    return (
+        <div style={{ padding: "20px" }}>
+            <Printer />
+        </div>
+    );
+}
