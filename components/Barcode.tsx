b11f3c091ad2de076a0bd344e80d6605eec26e7b
Kanghuan Xu <tommyxu@bu.edu>
2025-04-29 23:07:21 -0400

    adding css for phone view barcode

--- components/Barcode.tsx
+++ components/Barcode.tsx
@@ -1,17 +1,51 @@
+// Style by Kanghuan Xu
+
 "use client";
+
 import { toSVG } from "@bwip-js/generic";
 
-export default function Barcode({ data, rotate }: { data: string, rotate?: 0 | 1 | 2 | 3 }, isQR?: boolean) {
-    const svgString = toSVG({
-        bcid: isQR ? "qrcode" : "datamatrix",
-        text: data,
-        scale: 3,
-        rotate: (["R", "I", "N", "L",] as const)[rotate || 0]
-    });
+interface BarcodeProps {
+  data: string;
+  rotate?: 0 | 1 | 2 | 3;
+  isQR?: boolean;
+}
 
-    return (
-            <div
-                dangerouslySetInnerHTML={{ __html: svgString }}
-            />
-    );
+export default function Barcode({
+  data,
+  rotate = 0,
+  isQR = false,
+}: BarcodeProps) {
+  const svgString = toSVG({
+    bcid:   isQR ? "qrcode" : "datamatrix",
+    text:   data,
+    scale:  3,
+    rotate: (["R", "I", "N", "L"] as const)[rotate],
+  });
+
+  return (
+    <>
+      <div
+        className="barcode-inline"
+        dangerouslySetInnerHTML={{ __html: svgString }}
+      />
+      <style jsx>{`
+        .barcode-inline {
+          display: block;       
+          margin: 0 auto;      
+          width: 100%;
+          height: auto;        
+        }
+
+        /* on phones, make it full-width */
+        @media (max-width: 640px) {
+          .barcode-inline {
+          display: block;
+          margin: 0 auto;
+            width: 120%;
+            height: auto;
+          }
+        }
+      `}</style>
+    </>
+  );
 }
